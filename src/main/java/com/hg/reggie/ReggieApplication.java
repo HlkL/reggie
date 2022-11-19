36aacbe15e68e6203987301d722b528817dacc44
AL <AL@Jvava.cn>
2022-11-19 17:26:59 +0800

    springboot启动类

---
+++ src/main/java/com/hg/reggie/ReggieApplication.java
@@ -0,0 +1,24 @@
+package com.hg.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ *  ServletComponentScan 扫描webServlet原生注解
+ * @author HG
+ */
+@Slf4j
+@EnableTransactionManagement
+@SpringBootApplication
+@ServletComponentScan
+public class ReggieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class, args);
+        log.info("Reggie启动成功...");
+    }
+
+}
