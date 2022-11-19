a65b1f17803911b49e9e55f8c8b52550994ed91e
HlkL <1961154734@qq.com>
2022-11-19 22:31:41 +0800

    数据库实体类

---
+++ src/main/java/com/hg/reggie/entity/User.java
@@ -0,0 +1,41 @@
+package com.hg.reggie.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import java.io.Serializable;
+/**
+ * 用户信息
+ * @author HG
+ */
+@Data
+@TableName("user")
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+
+    //姓名
+    private String name;
+
+
+    //邮箱
+    private String phone;
+
+
+    //性别 0 女 1 男
+    private String sex;
+
+
+    //身份证号
+    private String idNumber;
+
+
+    //头像
+    private String avatar;
+
+
+    //状态 0:禁用，1:正常
+    private Integer status;
+}
