08d617af2e869d6e26be9f9d1b6fbed19e5998ab
HlkL <1961154734@qq.com>
2022-11-19 23:28:56 +0800

    菜品口味

---
+++ src/main/java/com/hg/reggie/service/impl/DishFlavorServiceImpl.java
@@ -0,0 +1,16 @@
+package com.hg.reggie.service.impl;
+
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hg.reggie.entity.DishFlavor;
+import com.hg.reggie.mapper.DishFlavorMapper;
+import com.hg.reggie.service.DishFlavorService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author HG
+ */
+@Service
+public class DishFlavorServiceImpl extends ServiceImpl<DishFlavorMapper, DishFlavor> implements DishFlavorService {
+
+}
